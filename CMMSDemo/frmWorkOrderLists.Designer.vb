2f86408daf303c0de94744d3e5f1745527947cd2
shyamprasath17 <shyamprasath17@gmail.com>
2013-09-25 15:20:12 +0530

    demoshyam

---
+++ CMMSDemo/frmWorkOrderLists.Designer.vb
@@ -0,0 +1,53 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmWorkOrderLists
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.dgvWo = New Telerik.WinControls.UI.RadGridView()
+        CType(Me.dgvWo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.dgvWo.MasterTemplate, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvWo
+        '
+        Me.dgvWo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvWo.Location = New System.Drawing.Point(0, 0)
+        Me.dgvWo.Name = "dgvWo"
+        Me.dgvWo.Size = New System.Drawing.Size(752, 365)
+        Me.dgvWo.TabIndex = 0
+        Me.dgvWo.Text = "RadGridView1"
+        '
+        'frmWorkOrderLists
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(752, 365)
+        Me.Controls.Add(Me.dgvWo)
+        Me.Name = "frmWorkOrderLists"
+        Me.Text = "WorkOrderList"
+        CType(Me.dgvWo.MasterTemplate, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.dgvWo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dgvWo As Telerik.WinControls.UI.RadGridView
+End Class
---
+++ CMMSDemo/frmWorkOrderLists.vb
@@ -0,0 +1,37 @@
+﻿Public Class frmWorkOrderLists
+    Dim dt As DataTable
+    Dim frmloaded As Boolean = False
+
+    Private Sub frmWorkOrderLists_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        frmloaded = False
+        createtable()
+        frmloaded = True
+    End Sub
+
+    Private Sub createtable()
+        dt = New DataTable()
+        dt.Columns.Add("WorkOrderNo", GetType(String))
+        dt.Columns.Add("ServiceType", GetType(String))
+        dt.Columns.Add("areaID", GetType(String))
+        dt.Columns.Add("status", GetType(String))
+
+        For i As Integer = 1 To 10
+            dt.Rows.Add(i.ToString(), "service " + i.ToString(), "102" + i.ToString(), "Approved")
+        Next
+        dgvWo.DataSource = dt
+    End Sub
+
+    Private Sub dgvWo_CellDoubleClick(sender As Object, e As Telerik.WinControls.UI.GridViewCellEventArgs) Handles dgvWo.CellDoubleClick
+        FormMain.RadDockMain.FloatWindow(FormMain.ToolWindowWo)
+    End Sub
+
+    
+
+    Private Sub dgvWo_CurrentRowChanged(sender As Object, e As Telerik.WinControls.UI.CurrentRowChangedEventArgs) Handles dgvWo.CurrentRowChanged
+        If (frmloaded) Then
+            If (Not dgvWo.CurrentRow.Cells("WorkOrderNo").Value Is Nothing) Then
+                FormMain.frmwo.fillworkorder(dt.Select("WorkOrderNo = '" & dgvWo.CurrentRow.Cells("WorkOrderNo").Value.ToString() & "'")(0))
+            End If
+        End If
+    End Sub
+End Class
