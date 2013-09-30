1a706af838f26162ca7eb109db284be524d524b9
shyamprasath17 <shyamprasath17@gmail.com>
2013-09-30 15:49:16 +0530

    shyamdemo

--- CMMSDemo/frmProjectList.vb
+++ CMMSDemo/frmProjectList.vb
@@ -1,5 +1,5 @@
 ﻿Public Class frmProjectList
-    Dim dt As DataTable
+    Dim dtProject As DataTable
     Dim frmloaded As Boolean = False
     Private Sub dgvProjectList_CellDoubleClick(sender As Object, e As Telerik.WinControls.UI.GridViewCellEventArgs) Handles dgvProjectList.CellDoubleClick
         FormMain.twProject.Show()
@@ -9,7 +9,13 @@
     Private Sub dgvProjectList_CurrentRowChanged(sender As Object, e As Telerik.WinControls.UI.CurrentRowChangedEventArgs) Handles dgvProjectList.CurrentRowChanged
         If (frmloaded) Then
             If (Not dgvProjectList.CurrentRow.Cells("ProjectNo").Value Is Nothing) Then
-                FormMain.frmProj.fillProject(dt.Select("ProjectNo = '" & dgvProjectList.CurrentRow.Cells("ProjectNo").Value.ToString() & "'")(0), dt)
+                FormMain.frmProj.fillProject(dtProject.Select("ProjectNo = '" & dgvProjectList.CurrentRow.Cells("ProjectNo").Value.ToString() & "'")(0), dtProject)
+                Dim dt_wo As DataTable = FormMain.frmwolst.dtWo.Copy()
+                dt_wo.DefaultView.RowFilter = "ProjectNo = '" & dgvProjectList.CurrentRow.Cells("ProjectNo").Value.ToString() & "'"
+                Dim dttask_copy As DataTable = FormMain.frmTskLst.dttask.Copy()
+                dttask_copy.Clear()
+                FormMain.frmTskLst.dgvTasks.DataSource = dttask_copy
+                FormMain.frmwolst.dgvWo.DataSource = dt_wo.DefaultView
             End If
         End If
     End Sub
@@ -23,16 +29,18 @@
         dgvProjectList.AllowDeleteRow = False
     End Sub
     Private Sub createtable()
-        dt = New DataTable()
+        dtProject = New DataTable()
         'dt.Columns.Add("Project", GetType(String))
         'dt.Columns.Add("WorkOrder", GetType(String))
-        dt.Columns.Add("ProjectNo", GetType(String))
+        dtProject.Columns.Add("ProjectNo", GetType(String))
         'dt.Columns.Add("ServiceType", GetType(String))
-        dt.Columns.Add("status", GetType(String))
+        dtProject.Columns.Add("status", GetType(String))
 
         For i As Integer = 1 To 10
-            dt.Rows.Add("Project " & i.ToString(), "Approved")
+            dtProject.Rows.Add("Project " & i.ToString(), "Approved")
         Next
-        dgvProjectList.DataSource = dt
+        dgvProjectList.DataSource = dtProject
+
     End Sub
+
 End Class
